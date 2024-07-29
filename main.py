0ac5cc5012d776de5ee17d8dfe96be5f94d04c16
Aftab Ahmad Khan <0201csds100@niet.co.in>
2024-07-29 12:46:19 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+start ="2015-01-01"
+today = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock prediction app")
+
+stocks = ("AAPL","GOOG","MSFT","GME")
+selected_stocks = st.selectbox("Select data set for prediction",stocks)
+n_years = st.slider("Years of prediction",1,4)
+period = n_years * 365
+
+@st.cache_data
+def load_data(ticker):
+    data=yf.download(ticker, start, today)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("load data...")
+data = load_data(selected_stocks)
+data_load_state.text("Loading data...done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'], name='stock_close'))
+    fig.layout.update(title_text = "Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
