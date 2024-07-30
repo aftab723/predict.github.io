1cbf8de370d5f3ec6930c603037fb55c4deb3e34
Aftab Ahmad Khan <0201csds100@niet.co.in>
2024-07-30 11:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,44 @@
-
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-start ="2015-01-01"
+start = "2015-01-01"
 today = date.today().strftime("%Y-%m-%d")
 
-st.title("Stock prediction app")
+st.title("Stock Prediction App")
 
-stocks = ("AAPL","GOOG","MSFT","GME")
-selected_stocks = st.selectbox("Select data set for prediction",stocks)
-n_years = st.slider("Years of prediction",1,4)
+stocks = ("AAPL", "GOOG", "MSFT", "GME")
+selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+n_years = st.slider("Years of prediction:", 1, 4)
 period = n_years * 365
 
 @st.cache_data
 def load_data(ticker):
-    data=yf.download(ticker, start, today)
+    data = yf.download(ticker, start, today)
     data.reset_index(inplace=True)
     return data
 
-data_load_state = st.text("load data...")
+data_load_state = st.text("Loading data...")
 data = load_data(selected_stocks)
-data_load_state.text("Loading data...done!")
+data_load_state.text("Loading data... done!")
 
 st.subheader('Raw data')
 st.write(data.tail())
 
 def plot_raw_data():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'], name='stock_open'))
-    fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'], name='stock_close'))
-    fig.layout.update(title_text = "Time Series Data", xaxis_rangeslider_visible=True)
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 plot_raw_data()
 
 # Predict forecast with Prophet.
-df_train = data[['Date','Close']]
+df_train = data[['Date', 'Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 m = Prophet()
